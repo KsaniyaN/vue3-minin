d158bca1100394e10fa05096ec7d8595a4f90ee5
XeniaN <xenia.novosilska@mirus.group>
2021-11-09 15:49:24 +1100

    * new methods, v-for, v-if, v-else, v-show, key, $refs, prevent, keyboard events

--- app2.js
+++ app2.js
@@ -1,6 +1,40 @@
 Vue.createApp({
+
 	data: () => ({
 		myHtml: '<h1>Vue3 App</h1>',
-		title: 'Cupcake ipsum Title'
-	})
+		title: 'Cupcake ipsum Title',
+		person: {
+			firstName: 'Xenia',
+			lastName: 'N',
+			age: 40
+		},
+		myItems: ['one', 2, 'three'],
+		myNumbers: [1, 2, 3, 4, 5, 6]
+	}),
+
+	computed: {
+		evenItems() {
+			return this.myNumbers.filter(i => i % 2 === 0)
+		}
+	},
+
+	methods: {
+		addNewListItem(event) {
+			//console.log(this.$refs.myInput.value);
+			if (this.$refs.myInput.value) {
+				this.myNumbers.unshift(this.$refs.myInput.value);
+				this.$refs.myInput.value = '';
+				console.log(event.key)
+			}
+		},
+
+		removeItem(i) {
+			this.myNumbers.splice(i, 1);
+		},
+
+		logItem(item) {
+			console.log('Log item: ', item);
+		}
+	}
+
 }).mount('#app');
